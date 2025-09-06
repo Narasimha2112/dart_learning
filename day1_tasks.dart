359ca7f5505bfa28ad0211afe02ae3bcd88957ec
Narasimha <satyansv5@gmail.com>
2025-09-06 13:08:20 +0530

    Day 1:  Task

---
+++ day1_tasks.dart
@@ -0,0 +1,27 @@
+// 📝 Practice Tasks (Day 1)
+
+// 1. Print your name, age, and favorite hobby in Dart.
+// 2. Declare variables: `temperature`, `isRaining`, `cityName` and print them.
+// 3. Use string interpolation to print a sentence like:
+//    `"Today in Hyderabad, the temperature is 30°C and it is not raining."`
+
+
+void main(){
+  String name = "Venkat";
+  int age  = 23;
+  String hobby = "Coding";
+
+  print("My name is $name,I am $age years old, and my hobby is $hobby.");
+
+  //Weather Example
+
+  var temparature = 30;
+  var isRaining = "not";
+  var cityName = "Hyderabad";
+  
+  print("Today in $cityName, the temprature is $temparature°C and it is $isRaining raining. ");
+}
+
+
+
+
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,5 @@
+ // var temparature = 30;
+  // var isRaining = "not";
+  // var cityName = "Hyderabad";
+  
+  // print("Today in $cityName, the temprature is $temparature°C and it is $isRaining raining. ");
